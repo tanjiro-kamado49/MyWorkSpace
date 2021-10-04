22d6947355d1adbaf6995c24fa35350541bbaedb
tanjiro-kamado49 <tanisha117435@gmail.com>
2021-10-04 14:55:48 +0530

    code

--- src/Algorithm/Sorting/QS.java
+++ src/Algorithm/Sorting/QS.java
@@ -1,6 +1,6 @@
 package Algorithm.Sorting;
 
-public class QS{
+public class QS {
     static void quick(int arr[], int lb, int ub) {
         int loc = 0;
         if (lb < ub) {
@@ -12,20 +12,21 @@ public class QS{
 
     static int partition(int[] arr, int lb, int ub) {
         int pivot = arr[ub];
-        int i = lb - 1;
+        int i = (lb - 1);
         for (int j = lb; j <= ub - 1; j++) {
 
             if (arr[j] < pivot) {
+
                 i++;
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[ub];
-        arr[ub] = temp;
-        return i + 1;
+        int temp = arr[ub];
+        arr[ub] = arr[i + 1];
+        arr[i + 1] = temp;
+        return (i + 1);
     }
 
     public static void main(String[] args) {
